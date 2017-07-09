ef9b23800ded20753a20b9b906daf170dcd75d80
Venkatayoganand <venkatayoganand.konda@gmail.com>
2017-07-09 13:31:37 +0530

    Add files via upload

---
+++ Finding Leapyear.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{int n;
+   Scanner s=new Scanner(System.in);
+   n=s.nextInt();
+
+if(n%4==0)
+    System.out.println("Leap year="+n);
+
+else 
+   System.out.println("Non Leap year");
+	}
+}
